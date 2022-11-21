1be4cd4de35ea5359f6c953ece678381cd22778b
Lei DONG <dongleivip@gmail.com>
2022-11-21 23:52:12 +0800

    axios fetch urls from aliyun FC

--- src/components/extractor.js
+++ src/components/extractor.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import parsePage from './pageParse';
+import getVideoUrls from './pageParse';
 
 class Extractor extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      // inputVal: 'https://mp.weixin.qq.com/s/nySwvV9DxZuj1hkAvBfF_g',
       inputVal: '',
-      result: '',
+      result: [],
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    const videoUrl = parsePage(this.state.inputVal)
-    this.setState({result: videoUrl});
+  async handleClick() {
+    const videoUrl = await getVideoUrls(this.state.inputVal)
+    // const videoUrl = ['Item 1', 'Item 2', 'Item 3'];
+    this.setState({ result: videoUrl });
   }
 
   render() {
@@ -21,13 +23,19 @@ class Extractor extends React.Component {
       <div>
         <p>输入网页链接：</p>
         <div>
-          <input value={this.state.inputVal} onChange={ (event) => {this.setState({inputVal: event.target.value})} } />
+          <input value={this.state.inputVal} onChange={(event) => { this.setState({ inputVal: event.target.value }) }} />
           <button onClick={this.handleClick}>解析</button>
         </div>
         <div>
-          <div style={this.state.result ? {} : { display: 'none' }} >
-            <div>链接</div>
-            {this.state.result}
+          <div style={this.state.result.length > 0 ? {} : { display: 'none' }} >
+            <div>优先使用包含"f10003"的链接</div>
+            <ul>
+              {
+                this.state.result.map((item, index) =>
+                  <li key={index}>{item}</li>
+                )
+              }
+            </ul>
           </div>
         </div>
       </div>
--- src/components/pageParse.js
+++ src/components/pageParse.js
@@ -1,35 +1,27 @@
-import { parse } from 'node-html-parser';
+const axios = require('axios').default;
 
-const parsePage = (url: string): string => {
+const SERVER_URL = 'https://getvideoaddress-get-vid-address-wmququrgyd.cn-hangzhou.fcapp.run'
 
-  if (!url) {
-    return 'url is not provided..';
+const getVideoUrls = async (url: string): string[] => {
+  if (!url || !url.startsWith("https://")) {
+    return ['请输入正确地址'];
   }
 
-  console.info('input url:', url);
-
-  const html = '<div data-v-cbdb9bbe="" class="js_inner inner not_fullscreen" >'
-    + '<div data-v-cbdb9bbe="" class="js_video_poster video_poster" style = "display: none;" >'
-    + '<video data-v-cbdb9bbe="" src = "http://mpvideo.qpic.cn/0b78nyaagaaa4yanabx3wbqfa3wdanxaaaya.f10003.mp4?dis_k=27298ec08821dfd456f92923851e8b60&amp;dis_t=1669031580&amp;vid=wxv_1971728112212115463&amp;format_id=10003&amp;support_redirect=0&amp;mmversion=false"'
-    +   'poster="http://mmbiz.qpic.cn/mmbiz_jpg/jicY1fLQseN5HxWlBqqTEg3ozqn0cJgZbhicBLRSzTh1NePrCX1EKQbFoMQ1rqYhoAicJPXF0jBnejER9Ggs866ZA/0?wx_fmt=jpeg" webkit - playsinline="isiPhoneShowPlaysinline" playsinline = "isiPhoneShowPlaysinline"'
-    +   'preload="metadata" crossorigin = "anonymous"'
-    +   'controlslist = "nodownload"'
-    +   'class="video_fill" >'
-    +   'Your browser does not support video tags'
-    +  '</video >'
-    + '</div>'
-    + '</div >';
-
-  const page = parse(html);
-  const section = page.querySelector('.video_fill');
-
-  if (!section) {
-    console.error('video section not fond..');
-    return '无法获取视频链接...';
+  try {
+    const options = {
+      headers: {
+        'Accept': 'text/json'
+      }
+    }
+    const requestEndpoint = `${SERVER_URL}?url=${url}`;
+    const response = await axios.get(requestEndpoint, options);
+    return response.data;
+  }
+  catch (error) {
+    console.error(error);
   }
 
-  const videoUrl = section.getAttribute('src');
-  return videoUrl;
+  return [];
 }
 
-export default parsePage;
+export default getVideoUrls;
