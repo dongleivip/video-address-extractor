187dce231a1ce92930e353afea1fad123c18b71c
Lei DONG <dongleivip@gmail.com>
2022-11-21 20:28:36 +0800

    Extract video url from hardcoded html

--- src/components/extractor.js
+++ src/components/extractor.js
@@ -24,16 +24,14 @@ class Extractor extends React.Component {
           <input value={this.state.inputVal} onChange={ (event) => {this.setState({inputVal: event.target.value})} } />
           <button onClick={this.handleClick}>解析</button>
         </div>
-
-
         <div>
           <div style={this.state.result ? {} : { display: 'none' }} >
             <div>链接</div>
             {this.state.result}
           </div>
         </div>
-
-      </div>);
+      </div>
+    );
   }
 }
 
