4e37cc31836bd587d841f4d129428370e185b4e1
dongleivip <dongleivip@gmail.com>
2022-11-23 11:02:28 +0000

    Deploying to gh-pages from @ dongleivip/video-address-extractor@fd757668cd842e6770fc2d359cd874304b5f4c7a 🚀

---
+++ precache-manifest.02a9e00e113fe2098ff0f35f7fb8241a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e9f1331bf76e12a3512",
+    "url": "./static/css/main.221a3428.chunk.css"
+  },
+  {
+    "revision": "1e9f1331bf76e12a3512",
+    "url": "./static/js/main.1e9f1331.chunk.js"
+  },
+  {
+    "revision": "7483025c10a0f8f622e0",
+    "url": "./static/js/1.7483025c.chunk.js"
+  },
+  {
+    "revision": "7389e2c9b6e9ede453a1",
+    "url": "./static/js/2.7389e2c9.chunk.js"
+  },
+  {
+    "revision": "51c24db6359cfb27c454",
+    "url": "./static/js/runtime~main.51c24db6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1823d597651f2abfe66e2fcd9282a4b9",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a3855dcd2ea7e3e4c55c9bb0c6fec36.js"
+  "./precache-manifest.02a9e00e113fe2098ff0f35f7fb8241a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
